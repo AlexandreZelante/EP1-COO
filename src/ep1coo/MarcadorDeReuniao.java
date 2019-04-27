6e93bbea883f83b7d661958d1eea3ffe9d92863c
Alexandre Zelante <alexandre.zelante@outlook.com>
2019-04-27 15:45:57 -0300

    Inserido intervalo de disponibilidade do participante

--- src/ep1coo/MarcadorDeReuniao.java
+++ src/ep1coo/MarcadorDeReuniao.java
@@ -28,8 +28,24 @@ public class MarcadorDeReuniao {
     
    public void indicaDisponibilidade(String participante, LocalDateTime inicio, LocalDateTime fim){
        //Verificar num if se a data encaixa no intervalo de data definida pelo organizador
+       // Codar aqui
+       
        ArrayList<Participante> listaDeParticipantes = novaReuniao.getParticipantes();
        //Adicionar uma busca para pegar o participante que possua o email recebido no parametro "participante"
-       //Pegando uma variavel tipo Participante, usar o inicio e fim (dos parametros) no método inserirDisponibilidade();
+       Participante participanteSelecionado = null;
+       for(Participante part : listaDeParticipantes){
+           if(part.email.equals(participante)){
+               //Participante encontrado
+               participanteSelecionado = part;
+               break;
+           }
+       }
+       if(participanteSelecionado != null){
+           //Participante existe -- Inclui um intervalo disponível dele
+           participanteSelecionado.inserirDisponibilidade(inicio, fim);
+           System.out.println("Intervalo inserido");
+       }else{
+           System.out.println("Participante não encontrado na lista da reunião");
+       }
    }
 }
